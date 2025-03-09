e8c3d1347be235a468644624a0af6c9bf9d78c15
Mariana Marlis <mbelisar@my.wctc.edu>
2025-03-09 09:25:54 -0500

    Added and registered Books List block

---
+++ build/blocks/books-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e5f4f05e2323be3ad490');
---
+++ build/blocks/books-list/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e30c8e81463d993d58e4');
